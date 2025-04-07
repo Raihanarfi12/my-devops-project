af7c6c408778c494e0abcb9eaac7fce8c3a45c6e
Raihanarfi12 <onlyraihan00@gmail.com>
2025-04-07 21:31:48 +0700

    Modify aws_ecs_service config

--- terraform-project/my-tf-project/modules/ecs/main.tf
+++ terraform-project/my-tf-project/modules/ecs/main.tf
@@ -1,3 +1,56 @@
 resource "aws_ecs_cluster" "my-ecs-cluster" {
   name = var.ecs_cluster_name
 }
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "my-app"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name      = "my-app"
+    image     = "nginx"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Effect    = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "app" {
+  name            = "app-service"
+  cluster         = aws_ecs_cluster.my-ecs-cluster.id
+  task_definition = aws_ecs_task_definition.app.arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+
+  network_configuration {
+    subnets          = aws_subnet.public[*].id
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_sg.id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_policy]
+}
